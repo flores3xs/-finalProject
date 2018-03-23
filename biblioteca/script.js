a74ee4cd7a94d1c84958a8f75d3e3b532158625d
flores3xs <flores3xs@gmail.com>
2018-03-23 13:20:31 -0400

    Friday March 23

--- biblioteca/script.js
+++ biblioteca/script.js
@@ -3,3 +3,12 @@ console.log("#pasoFino")
 // modal box for desktop
 // bottom modal for cel phones 
 // header & footer modal (optional)
+
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal("show");
+    });
+        $("#myModal").on('hidden.bs.modal', function () {
+            alert('"V for Victory"(Winston Churchill) #pasoFino');
+    });
+});
--- main.js
+++ main.js
@@ -7,8 +7,14 @@ $(document).ready(function(){
 // modal box for desktop
 // bottom modal for cel phones 
 // header & footer modal (optional)
-//CSS/JS modal
-//https://www.w3schools.com/howto/howto_css_modals.asp
-//J-Query Modal
-//https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp
+// (modal) Methods: show & hide  - Events: show & hide
 
+
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal("show");
+    });
+        $("#myModal").on('hidden.bs.modal', function () {
+            alert('"V for Victory"(Winston Churchill) #pasoFino');
+    });
+});
